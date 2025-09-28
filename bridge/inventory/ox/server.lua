9a6d4c2dc652eab58ab3ac19594d66fc43dab4ff
Akyolm383 <ramazanakyol161@gmail.com>
2025-09-28 18:26:31 +0300

    chore: add inventory management functions to bridge modules
    - Added `CreateShop` and `CreateStash` functions for `ox` and `qb` inventory systems.
    - Included error handling for invalid data inputs.

--- bridge/inventory/ox/server.lua
+++ bridge/inventory/ox/server.lua
@@ -1,6 +1,15 @@
 local OxInventory = {}
 
 
+function OxInventory.CreateShop(id, data)
+    if not id or not data then lib.print.error("Invalid shop data") return end
+    exports.ox_inventory:RegisterShop(id, data)
+end
+
+function OxInventory.CreateStash(data)
+    if not data then lib.print.error("Invalid stash data") return end
+    exports.ox_inventory:RegisterStash(data)
+end
 
 
 
--- bridge/inventory/qb/server.lua
+++ bridge/inventory/qb/server.lua
@@ -1,6 +1,29 @@
 local QbInventory = {}
 
 
+function QbInventory.CreateShop(id, data)
+    if not id or not data then lib.print.error("Invalid shop data") return end
+
+    exports['qb-inventory']:CreateShop({
+        name = id,
+        label = data.label or "Shop",
+        slots = #data.inventory or 30,
+        coords = data.locations.coords,
+        items = data.inventory or {}
+    })
+
+end
+
+function QbInventory.CreateStash(data)
+    if not data then lib.print.error("Invalid stash data") return end
+
+    exports['qb-inventory']:CreateInventory(data.id,{
+        label = data.label or "Stash",
+        maxweight = data.weight or 1000000,
+        slots = data.slots or 30,
+    })
+    
+end
 
 
 
